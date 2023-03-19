029346b62dd45f983857794cb909fc0f69eb6f1d
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2023-03-19 11:27:37 +0100

    Remove redundant setting to null of local variable wantToDoDescription inside DisplayDiaryRecord.build
    because that's null by default.

--- i-care.by-app/lib/display_diary_record.dart
+++ i-care.by-app/lib/display_diary_record.dart
@@ -244,8 +244,6 @@ class DisplayDiaryRecord extends StatelessWidget {
           String? wantToDoDescription;
           if (i < wantToDoForSeveral.length) {
             wantToDoDescription = wantToDoForSeveral[i];
-          } else {
-            wantToDoDescription = null;
           }
 
           String? emotionsAndFeelingsOnWantedToDo;
