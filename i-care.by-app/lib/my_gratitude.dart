4d6a0c5cb4a08b2e79e085c0a69e0accecd4eedd
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2023-08-15 09:35:50 +0200

    The 29 version of the current reality tree
    Also record my gratitude to the person, who offered to change
    wording in the introducation text for CRT about dictatorship in
    Belarus.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2023_08_15_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2023_08_08_01,
               ),
               TextSpan(
