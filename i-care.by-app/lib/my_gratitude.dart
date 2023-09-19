1e9479103668303228d35ba3eab84390ff445081
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2023-09-19 10:29:45 +0200

    The 100 version of the current reality tree
    Also record my gratitude to the persons, who provided feedback
    about the "balance/equilibrium in relationships between
    athorities and society" mentioned in previous versions of CRT.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,12 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2023_09_19_02,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2023_09_19_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2023_09_10_01,
               ),
               TextSpan(
