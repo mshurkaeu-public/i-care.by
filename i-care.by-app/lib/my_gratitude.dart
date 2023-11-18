38fd57b8e8df18ed34b9938902dfd5fb5ad0d28d
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2023-11-18 10:28:17 +0100

    The 215 version of the current reality tree
    Also record my gratitude to the person, who commented on my
    confidence about existence of Russian forces, which help Belarusian
    citizens to change status quo in relationships with authorities.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2023_11_18_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2023_11_15_01,
               ),
               TextSpan(
