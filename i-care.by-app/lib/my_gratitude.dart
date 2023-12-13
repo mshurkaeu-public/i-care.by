4bb7ac52fc3045d3f7050911078431b110cc3d18
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2023-12-13 14:38:59 +0100

    The 241 version of the current reality tree
    Start introducation of a separate CRT about society to make understanding
    of the second CRT about one citizen easier.
    
    Also record my gratitude to the person, who provided feedback that
    helped me to notice a logical error and to understand a need for the
    introductory CRT about society.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -22,6 +22,15 @@ class MyGratitude extends StatelessWidget {
           TextSpan(
             text: l10n.aboutMyGratitudeIntroductory,
             children: [
+              // 2023-12
+              TextSpan(
+                text: l10n.aboutMyGratitude_2023_12_title,
+                style: titleStyle,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2023_12_13_01,
+              ),
+
               // 2023-11
               TextSpan(
                 text: l10n.aboutMyGratitude_2023_11_title,
