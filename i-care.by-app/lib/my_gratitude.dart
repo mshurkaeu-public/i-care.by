77512a2865cd2ad17f26cd773b404e9cbf1beafd
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-01-22 21:31:13 +0100

    Record my gratitude to Google "ARB Editor" extension for VS Code team
    for creation of the extension v0.0.12, which helped me find errors
    in my arb file.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_01_21_05,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_01_21_04,
               ),
               TextSpan(
