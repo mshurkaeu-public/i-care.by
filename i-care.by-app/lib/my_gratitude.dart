ad7125551a17195b666dad53b22c95d7698bca6c
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-01-22 21:08:43 +0100

    Record my gratitude to Flutter community
    for creation of new versions of Flutter packages, which I use during
    my application development:
    _fe_analyzer_shared (61.0.0 ⟶ 64.0.0),
    analyzer (5.13.0 ⟶ 6.2.0),
    collection (1.17.1 ⟶ 1.18.0),
    coverage (1.6.3 ⟶ 1.7.2),
    flutter_lints (2.0.2 ⟶ 2.0.3),
    http (1.1.0 ⟶ 1.2.0),
    intl (0.18.0 ⟶ 0.18.1),
    matcher (0.12.15 ⟶ 0.12.16),
    material_color_utilities (0.2.0 ⟶ 0.5.0),
    meta (1.9.1 ⟶ 1.10.0),
    package_info_plus (4.1.0 ⟶ 4.2.0),
    path_provider (2.1.0 ⟶ 2.1.2),
    platform (3.1.0 ⟶ 3.1.4),
    plugin_platform_interface (2.1.5 ⟶ 2.1.8),
    source_span (1.9.1 ⟶ 1.10.0),
    stack_trace (1.11.0 ⟶ 1.11.1),
    stream_channel (2.1.1 ⟶ 2.1.2),
    test (1.24.1 ⟶ 1.24.9),
    test_api (0.5.1 ⟶ 0.6.1),
    test_core (0.5.1 ⟶ 0.5.9),
    url_launcher (6.1.12 ⟶ 6.2.3),
    vm_service (11.9.0 ⟶ 13.0.0),
    web ( ⟶ 0.3.0),
    webkit_inspection_protocol (1.2.0 ⟶ 1.2.1),
    win32 (5.0.6 ⟶ 5.2.0),
    xdg_directories (1.0.2 ⟶ 1.0.4)

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_01_21_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_01_21_01,
               ),
 
