cc4af2140ff4eb22bb9690472c1b6b45371c3d0f
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-05 16:34:55 +0100

    Record my gratitude to the person, who influenced me and I started to read Stephen R. Covey's book "The 7 Habits of Highly Effective People"

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_04_04,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_04_03,
               ),
               TextSpan(
