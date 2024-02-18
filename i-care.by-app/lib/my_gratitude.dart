9bc4ddbae1c9e7b00aa12a9dda318527010598ac
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-18 17:38:58 +0100

    Modify statement of the 2nd basic hypothesis
    I realized a need for the "a citizen is conscious" or another
    statement like that to discuss if the citizen knows that something
    is required to do. If one is unconscious then it is difiicult to
    judge if the person knows something or not.
    
    That realization happened to me approximately 10 days ago, while I
    was working on the causes why a citizen cannot do something even
    though the citizen knows it is required to do/may be done and knows
    how to do that.
    
    Also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of https://draw.io version 23.1.5. I use that web
    application to draw CRT for dictatorship in Belarus.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_18_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_18_01,
               ),
               TextSpan(
