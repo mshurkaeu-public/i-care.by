9c45bf9d67b3e5cadb1db8d2cac5fb6d5b185202
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-29 17:31:21 +0100

    Ran `flutter pub upgrade` command
    Also record my gratitude to flutter.dev team for creation of package
    url_launcher version 6.2.5.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_29_04,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_29_03,
               ),
               TextSpan(
