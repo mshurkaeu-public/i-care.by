b1e765d0e4679abb2d5e1bbe94f53df77e9c3e7e
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-29 17:21:22 +0100

    Upgrade Flutter from version 3.16.9 to 3.19.2
    Also record my gratitude to Flutter team for creation of Flutter
    version 3.19.2 and record my gratitude to Dart Team for creation
    of path 1.9.0 package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,12 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_29_03,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_29_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_29_01,
               ),
               TextSpan(
