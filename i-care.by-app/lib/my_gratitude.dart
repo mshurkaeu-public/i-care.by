514a77906e766cfe92c0dfa64c2190de744eb02d
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-29 16:57:41 +0100

    Record my gratitude to MS VS Code team
    for creation of VS Code 1.87.0. I use that to write code in Flutter.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_29_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_22_01,
               ),
               TextSpan(
