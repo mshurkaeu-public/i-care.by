abeac9c3be0649dcf4ae6a95128e3be567fe5704
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-02-29 21:37:15 +0100

    Add a link to hypothesis 12 https://github.com/mshurkaeu-public/i-care.by/discussions/15 into CRT description
    also record my gratitude to JGraph Ltd and draw.io AG teams for
    https://draw.io update.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_02_29_06,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_02_29_05,
               ),
               TextSpan(
