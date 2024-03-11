7a3d67d9b8660d0019d7e84818ef6a2d392ccbec
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-03-11 19:50:44 +0100

    Record my gratitude to the author of hypothesis https://github.com/mshurkaeu-public/i-care.by/discussions/18

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_03_11_03,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_03_11_02,
               ),
               TextSpan(
