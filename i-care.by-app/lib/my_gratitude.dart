efd5fa62b7124d494095433b5a18bdb556ae1c56
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-03-18 18:05:00 +0000

    Replace word "уздзейнічае" with word "уплывае" in the introductory text of CRT
    to make text easier to understand. And record my gratitude to the
    person, who propsed the modification.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_03_18_04,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_03_18_03,
               ),
               TextSpan(
