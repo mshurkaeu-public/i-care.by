42e2ae8e8f1db1aff993429aa65901b28a927413
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-03-30 19:01:28 +0100

    Fix couple of typos in the description of basic hypotheses for CRT about dictatorship
    also record my gratitude to the person, who reported the typos
    and record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.2.1 of https://draw.io website.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,12 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_03_30_04,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2024_03_30_03,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_03_30_02,
               ),
               TextSpan(
