0febee4a19d31a2b16bf3183477956f2559a43ba
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-04-30 15:07:01 +0200

    Upgrade photo_view package to version 0.15.0
    and also record my gratitude to blue-fire.xyz team for the creation
    of this version of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_04_30_03,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_04_30_02,
               ),
               TextSpan(
