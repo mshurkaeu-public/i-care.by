978451e3b55aecdc75f89cbd83b988d98be4a277
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-05-03 10:31:07 +0200

    Replace a link to the poll about the most important person in your life
    the new poll was started in January 2024 and already has almost 4
    times more responses than the previous one.
    
    Also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.3.1 of https://draw.io website.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_05_03_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_05_03_01,
               ),
 
