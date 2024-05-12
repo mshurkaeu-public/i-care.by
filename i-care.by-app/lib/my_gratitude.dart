752d292a7cada226275629e769a3e88d9ee158e7
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-05-12 15:17:13 +0200

    Move "Attack. Tries to destroy some of the obstacles from the Bₙ set of obstacles" node and its children 160 points up
    to free up vertical space to insert children of node "Escape. Tries
    to workaround some of the obstacles from the Bₙ set of obstacles".
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.4.0 of website draw.io.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_05_12_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_05_10_01,
               ),
               TextSpan(
