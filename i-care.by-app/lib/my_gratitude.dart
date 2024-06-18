963f8fc7e6d2d13d0700366d9ae73b94d6ee2e6e
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-06-18 16:20:45 +0200

    Amend text of some of the causes of "wants to achieve the initial goal and doesn't try to change the reality"
    to describe the situation more explcitly. The same/similar effect
    could be achieved with arrows from the corresponding nodes but that
    would complicated the diagram. Maybe later. Will see.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.5.5 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_06_18_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_06_14_01,
               ),
               TextSpan(
