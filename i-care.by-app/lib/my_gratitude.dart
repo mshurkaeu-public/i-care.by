c6b56f8bb91d9d81ee07e3a536c0234dd8d1b3ec
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-06-22 13:34:41 +0200

    Upgrade multi_split_view Flutter package to version 3.2.1
    just to use the latest version of the package.
    
    And also record my gratitude to Carlos Eduardo Leite de Andrade for
    creation of version 3.2.1 of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_06_22_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_06_22_01,
               ),
               TextSpan(
