b00a7768086bf4a20acb21b1a685e57488abcb40
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-06-26 12:22:41 +0200

    Introduce a logical group "all (? all ?) possible ways to change the reality to achieve a goal"
    I understood that this group would be helpfull to deeper review
    "causes" of "doesn't know it is required to try to change the reality
    to achieve the initial goal" node.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_06_26_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_06_23_03,
               ),
               TextSpan(
