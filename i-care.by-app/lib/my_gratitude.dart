7ef1777a87d1f38ec484113216ae20418baaf4b0
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-07-12 15:07:59 +0200

    Amend title of the column of the reasons why a citizen, who want to achieve the initial goal doesn't know that it's required to try to change the reality to achieve the goal
    to explicitly mention that the list of the reasons must be not known
    to the citizen all together at the same moment.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.6.5 of the website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_07_12_03,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_07_12_02,
               ),
               TextSpan(
