12f2e19b6563e1087cf67a3077b91ca5dce560f5
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-08-20 16:02:08 +0200

    Amend text of "absolutely sure that he knows enough to be absolutely sure that he cannot change the reality in the direction of performing of action D" node
    to explicitly mention "absolute confidence". Without the "absolute
    confidence" the citizen would be in the different point of the
    map of the reality.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.7.7 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_08_20_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_08_10_01,
               ),
               TextSpan(
