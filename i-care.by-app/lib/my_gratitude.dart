8cc15c145ff7096396a8d633b79539476f7b8f2f
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-09-06 15:10:01 +0200

    Move verything on the right under description of why a person cannot do something 1160 points down
    to free up vertical space to expand the mentioned description when
    needed.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.7.12 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_09_06_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_09_06_01,
               ),
               TextSpan(
