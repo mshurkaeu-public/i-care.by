ff4467e0f9b865767b18935bb86d193acd224826
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-09-12 09:57:40 +0200

    Amend a comment for "doesn't try to chage reality" node
    to reflect my current understanding of reality better.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.7.13 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_09_12_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_09_11_01,
               ),
               TextSpan(
