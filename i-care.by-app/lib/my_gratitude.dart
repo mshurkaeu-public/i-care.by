58f2960520d6932cea6fcb4cc4383d3e56246546
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-09-15 15:58:05 +0200

    Move "knows it required/possible to try to change reality but doesn't want to" node 960 points down
    to center the node vertically against it's "causes".
    
    Also record my gratitude to 2024-09-15 JGraph Ltd and draw.io AG
    teams for creation of version 24.7.14 of website
    https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_09_15_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_09_13_02,
               ),
               TextSpan(
