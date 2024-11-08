9c9aec5cc188119e00ae2ab4dc66e0d3a56dd05b
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-11-08 20:07:37 +0100

    Upgrade Flutter package package_info_plus to version 8.1.1
    just because the new version is available. And record my gratitude
    to Flutter community for creation of the version of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_11_08_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_11_08_01,
               ),
 
