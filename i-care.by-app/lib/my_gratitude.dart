6fe5d077a27772e91f9f12fe9e8a19f87f17a933
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-11-23 10:01:47 +0100

    Introduce a logical group for all (? all ?) possible types of situations when a citizen somehow impacts officials and special forces
    to prepare the diagram to extract "statement 5" to discuss with
    other people.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.8.9 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_11_23_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_11_22_01,
               ),
               TextSpan(
