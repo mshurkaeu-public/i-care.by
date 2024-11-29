d33090dd91ff45e01fcb46b70ffddc30e01043fa
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-11-29 18:53:15 +0100

    Fix a typo on CRT
    and record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 24.9.3 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_11_29_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_11_26_02,
               ),
               TextSpan(
