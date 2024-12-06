8ede9cbbeb7e6a49f561df4ae29d8493eb3b5fd0
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-12-06 22:24:26 +0100

    Introduce a logical group for cases of citizen's impact when the citizen has a requirement to officials and special forces
    this finalizes the split into two logical layers.
    
    And record my gratitude to the person, who provided me feedback
    that the statement 5 (according to that moment numeration) was
    difficult to understand.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_12_06_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_12_06_01,
               ),
               TextSpan(
