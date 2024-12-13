acf5752ba072b63ab59a0b16515f577e1d05a6ec
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-12-13 22:16:50 +0100

    Upgrade Flutter to version 3.27.0
    and record my gratitude to Flutter team for creation of this version
    of Flutter.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_12_13_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_12_13_01,
               ),
               TextSpan(
