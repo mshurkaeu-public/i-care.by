531bde12ca8b1b1e5b86da8c81319e12e7b7c0aa
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-12-15 08:38:58 +0100

    Introduce short description of 1st level "causes" of "a person knows that it's possible/required to try to change reality in the direction of performing of action D but doen't want to try" node
    I kind of wanted to express that thought long time ago, but required
    words didn't come. 🤷 A conversation with a person about imaginary
    person SP₂ helped me to finally realize what I want to express and
    to find the words.
    
    Record my gratitude to the person, who discussed the topic with me.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_12_15_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_12_13_03,
               ),
               TextSpan(
