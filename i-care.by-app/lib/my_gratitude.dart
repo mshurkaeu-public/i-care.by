de54bd9e267f6f0fc1f2acf573cd58b0203231a6
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2024-12-17 20:11:08 +0100

    Upgrade Flutter to version 3.27.1
    just because the new version is available. And record my gratitude
    to Flutter team for creation of the version.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2024_12_17_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2024_12_15_01,
               ),
               TextSpan(
