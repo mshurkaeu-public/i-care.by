d137cfb314910c377435b6cbbf17f144cbac3195
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-01-16 21:55:17 +0100

    Record my gratitude to Microsoft VS Code team
    for creation of version 1.96.3 of VS Code.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_01_16_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_01_10_01,
               ),
               TextSpan(
