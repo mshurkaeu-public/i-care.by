b821f5c65def128ce97d7b9e5c0be09d739c267b
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-01-16 23:13:25 +0100

    Describe essence of having a requirement to `officials and special forces` via execution of will
    since dictatorship is a situation when one party subordinates another
    party to it's will then to find a root cause of the effect I need to
    find a way to describe processes on both sides via execution of will.
    Sounds simple, but somehow doesn't happen fast and easy. I guess,
    I have not enough intuition to do this fast. Thus I do it slowly :)
    and build better understanding of reality during the provess.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 26.0.6 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_01_16_04,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_01_16_03,
               ),
               TextSpan(
