0f4c4d1682cd1ebfc01c70d01df9b11d039292bc
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-01-25 11:24:34 +0100

    Amend titles for pictures with parrots
    to make it more explicit what exactly are the situations illustrated
    on the pictures.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 26.0.8 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_01_25_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_01_25_01,
               ),
               TextSpan(
