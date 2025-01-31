2e3e0f4bdeb71304991b621c66b35e1a4b2d82c3
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-01-31 23:32:09 +0100

    Add a link to a new hypothesis https://github.com/mshurkaeu-public/i-care.by/discussions/31
    and also record my gratitude to the author of the hypothesis.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_01_31_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_01_30_02,
               ),
               TextSpan(
