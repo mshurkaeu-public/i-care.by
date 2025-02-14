eb04de00327d91b8f92f68337d61d0a5dcb9a138
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-02-14 22:50:39 +0100

    Modify text around statement 1
    to use explicit question to the reader with a red question mark
    right after the end of my statement. To make it very explicit where
    the end of the statement is and to make it very explicit that I
    expect feedback (agree or don't) .
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 26.0.13 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_02_14_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_02_13_02,
               ),
               TextSpan(
