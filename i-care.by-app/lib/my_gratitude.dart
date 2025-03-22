611a9546fc7ec4e321e2d7f600bfe03dea68562f
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-03-22 10:56:09 +0100

    Insert new statement 6
    to describe a situation when a reader knows about own mistake in
    own thoughts about reality. That helped me to realize that just ask
    about agreement is not enough. I have to explicitly ask about
    presence of mistakes in my statements.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 26.1.1 of website https://app.diagrams.net/.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_03_22_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_03_15_01,
               ),
               TextSpan(
