c65eb5043df0e540c48f6eb85a2af590ef7546dc
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-04-04 23:25:55 +0200

    Make statement 7 more precise
    I don't care if the readers don't make their decisions themselves.
    What I actually care about in this statement is that required
    "sufficient match" theoretically may be less than 100%.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation on version 26.2.6 of website https://app.diagrams.net/.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_04_04_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_04_04_01,
               ),
 
