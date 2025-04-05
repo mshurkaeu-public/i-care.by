6dd8ae69e84dd89c9bcf1937fd12bd14dea90606
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-04-05 13:38:03 +0200

    Add a comment to statement 7
    to describe a possible existance of a person, who agrees with some
    my statement with close to 0% match of our thoughts about reality.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 26.2.7 of website https://app.diagrams.net/.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_04_05_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_04_04_02,
               ),
               TextSpan(
