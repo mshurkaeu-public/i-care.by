82f3de9e49d1bae833bd9e773f19cb2997e68da4
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-05-10 16:21:34 +0200

    Insert new statement 12
    to prepare explanation of why the articles in explanatory dictionaries
    for word "адносіны" is "porridge". Maybe the deeper cause of that was
    "porridge" in heads of Belarusians. 🤷 I hope I'll figure out this
    eventually.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for creation
    of version 27.0.3 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_05_10_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_05_10_01,
               ),
 
