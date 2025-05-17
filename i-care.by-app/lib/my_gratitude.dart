de6866ab19c934301ecf5ead48a3bff6a2829e3d
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-05-17 16:52:19 +0200

    Amend the separate question text in the end of the Introduction chapter
    and record my gratitude to JGraph Ltd and draw.io AG teams for creation
    of version 27.0.6 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_05_17_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_05_17_01,
               ),
               TextSpan(
