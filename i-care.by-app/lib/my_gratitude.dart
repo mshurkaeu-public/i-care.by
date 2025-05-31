530a6e5772f5d812c15c6ffb50c71c7cd0403fcc
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-05-31 22:34:04 +0200

    Amend statement 14
    I believe this modification makes it easier to read and understand.
    
    Also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 27.1.1 of website https://app.diagrams.net/.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_05_31_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_05_30_01,
               ),
               TextSpan(
