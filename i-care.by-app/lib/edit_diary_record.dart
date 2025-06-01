mshurkaeu-public/i-care.by

commit 37a584d353eee3daa8471304f8bad82f8fa8e6ed

Fix a bug that a double tap on horisontal divider caused a reset of vertical divider position

i-care.by-app/lib/edit_diary_record.dart
@@ -1179,14 +1179,8 @@ class _TwoAreasSplitViewState extends State<_TwoAreasSplitView> {
   }
 
   void _restoreDefaultColumnsWeights() {
-    _multiSplitViewController.areas = [
-      Area(
-        flex: widget.initialFirstAreaWeight,
-      ),
-      Area(
-        flex: (1 - widget.initialFirstAreaWeight),
-      ),
-    ];
+    _multiSplitViewController.areas[0].flex = widget.initialFirstAreaWeight;
+    _multiSplitViewController.areas[1].flex = 1 - widget.initialFirstAreaWeight;
   }
 
   @override

commit 11c122e5778ce050e41158b764844bf68cbf0f73

Fix a regression introduced with commit 37a584d353eee3daa8471304f8bad82f8fa8e6ed
new implementation of _restoreDefaultColumnsWeights could not be
called from initState. Because the areas were not initialized
properly yet in that point. Thus starting the application and
opening a diary record would show a blank screen (there was an
exception).

i-care.by-app/lib/edit_diary_record.dart
@@ -1215,7 +1215,10 @@ class _TwoAreasSplitViewState extends State<_TwoAreasSplitView> {
   void initState() {
     super.initState();
 
-    _restoreDefaultColumnsWeights();
+    _multiSplitViewController.areas = [
+      Area(flex: widget.initialFirstAreaWeight),
+      Area(flex: (1 - widget.initialFirstAreaWeight)),
+    ];
   }
 }
 