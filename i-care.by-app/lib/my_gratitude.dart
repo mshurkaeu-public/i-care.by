93d2d26f6a98ef69d813a6093ca392b612f41fdc
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-06-28 15:27:48 +0200

    Record my gratitude to Flutter team for creation of version 3.32.5 of Flutter

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_06_28_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_06_20_02,
               ),
               TextSpan(
