8046284a6e3a52de1f2ff89008d5d1fdf0057c39
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-07-31 15:08:46 +0200

    Fix spelling error in the questions going right after each statement
    and record my gratitude to the person, who reported me that error
    and also a couple of other errors which I corrected in two previous
    commits.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_07_31_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_07_31_01,
               ),
               TextSpan(
