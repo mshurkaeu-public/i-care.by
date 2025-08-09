2c16ceeef6f2bfeb7c0266a4a122fd70d01e7eee
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-08-09 12:24:41 +0200

    Amend announcement 5 and split that to three announcements: 5, 6, 7
    to better prepare a reader to understand statement 4 goals.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 28.0.9 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_08_09_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_08_08_02,
               ),
               TextSpan(
