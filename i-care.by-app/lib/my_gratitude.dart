5739e933f0535d088e696ac131d85b615f7d0e2d
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-08-23 14:42:17 +0200

    Move announcement 27 to be right before statement 43
    this also caused a change of the announcement number 27 ⟶ 28.
    I did that to have the explanations about relationships state near
    the statement about relationships state. Before that the announcement
    was "too far" from the statement.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 28.1.1 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_08_23_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_08_20_01,
               ),
               TextSpan(
