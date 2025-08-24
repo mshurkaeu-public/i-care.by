f9762f5d6c5f65be27be16dc39dc01ca63e95d55
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-08-25 00:09:54 +0200

    Re-work statement 7 to be simpler: introduce statements 7, 8, 9 instead of just 7
    and record my gratitude to the person, who provided feedback about
    complexity of the previous version of statement 7.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_08_24_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_08_23_01,
               ),
               TextSpan(
