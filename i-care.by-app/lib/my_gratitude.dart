ea2ce637d93fc4b8e312778844ad939c38e550cc
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-08-29 13:41:33 +0200

    Fix punctuation error in the comment to statement 9
    and record my gratitude to the person who reported spelling errors
    in texts related to statement 9 and 10.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_08_29_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_08_28_02,
               ),
               TextSpan(
