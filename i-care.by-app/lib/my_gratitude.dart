c29851ec562842f5293fdd1f2871318526ac3e4c
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-09-13 21:55:09 +0200

    Replace statement 16 with "smaller" statements 16, 17, 18, 19, 20
    because I received feedback from a couple of people that original
    statement 16 is not clear enough to them. And record my gratitude to
    the person who reported the issue first. The second person reported
    the issue after I decided to split the statement into smaller steps.
    
    And also record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 28.2.1 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,12 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_09_13_02,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2025_09_13_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_09_12_02,
               ),
               TextSpan(
