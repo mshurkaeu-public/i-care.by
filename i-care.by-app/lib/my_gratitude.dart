0420439662180035492dfaf5d1e29ba61c98a307
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-10-12 14:51:28 +0200

    Fix spelling errors in comments to statement 36
    and record my gratitude to the person who reported the errors.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_10_12_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_10_10_01,
               ),
               TextSpan(
