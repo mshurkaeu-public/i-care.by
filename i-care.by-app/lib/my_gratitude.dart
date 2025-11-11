9e2e110db284ff06e4e454a7f5f872167152595a
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-11-11 15:07:41 +0100

    Fix spelling errors in comments to statement 23 and in announcement 27, also modify visual style of logical "and" on diagrams
    and record my gratitude to the person who reported the errors and
    provided feedback.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_11_11_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_11_10_02,
               ),
               TextSpan(
