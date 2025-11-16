bf344289bf5559b0b8c560d49a6025294f377a43
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2025-11-16 15:27:50 +0100

    Introduce a new comment to statement 21
    to explain that to have a thought is also an effect of some cause
    in reality.
    
    And record my gratitude to JGraph Ltd and draw.io AG teams for creation
    of version 29.0.3 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2025_11_16_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2025_11_15_01,
               ),
               TextSpan(
