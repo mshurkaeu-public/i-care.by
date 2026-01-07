25ef3b2525362b6f42ee6770b15f5d63cd1d6889
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-01-07 22:37:55 +0100

    Fix spelling error in a comment to statement 38
    and record my gratitude to JGraph Ltd and draw.io AG teams for
    creation of version 29.2.10 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -22,6 +22,15 @@ class MyGratitude extends StatelessWidget {
           TextSpan(
             text: l10n.aboutMyGratitudeIntroductory,
             children: [
+              // 2026-01
+              TextSpan(
+                text: l10n.aboutMyGratitude_2026_01_title,
+                style: titleStyle,
+              ),
+              TextSpan(
+                text: l10n.aboutMyGratitude_2026_01_07_01,
+              ),
+
               // 2025-12
               TextSpan(
                 text: l10n.aboutMyGratitude_2025_12_title,
