7acbbfa9b60d492d68a711f10682e0c0e6d24fe4
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-01-20 22:43:29 +0100

    Amend formatting of announcement 1
    to make it a bit easier to read. And record my gratitude to
    JGraph Ltd and draw.io AG teams for creation of version 29.3.4 of
    website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_01_20_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_01_17_01,
               ),
               TextSpan(
