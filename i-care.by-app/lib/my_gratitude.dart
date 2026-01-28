51bb714382e89e98bdc87f2ae86b3f344a856f44
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-01-28 23:59:01 +0100

    Introduce new statement 52 and announcement 55 with picture 3
    and record my gratitude to JGraph Ltd and draw.io AG teams fo creation
    of version 29.3.6 of website https://app.diagrams.net.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_01_28_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_01_24_01,
               ),
               TextSpan(
