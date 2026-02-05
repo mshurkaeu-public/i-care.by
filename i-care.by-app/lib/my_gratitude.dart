04f431ccecca71ecaa8cb27c7ccb036f9b598649
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-02-05 21:43:34 +0100

    Upgrade Flutter package carousel_slider to version 5.1.2
    just because the new version is avaliable. And record my gratitude
    to serenader.me for creation of the version of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_02_05_02,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_02_05_01,
               ),
 
