11c0442cfbc691d0d2be0751a1a682fec1ef3030
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-02-21 09:33:43 +0100

    Upgrade Flutter package google_fonts to version 8.0.2
    just because the new version is available. And record my gratitude
    to core Flutter team for creation of the version of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -28,6 +28,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_02_21_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_02_20_03,
               ),
               TextSpan(
