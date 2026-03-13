fc1bf760d7a0ee97ab079f8b868bb5b5f4c725e1
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-03-13 14:34:48 +0100

    Fix spelling issues in comments to the statement 22
    And record my gratitude to the person who reported the issues.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -44,6 +44,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_03_13_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_03_09_03,
               ),
               TextSpan(
