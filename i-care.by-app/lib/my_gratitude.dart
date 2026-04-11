916a6c0944a60f1234f4f52210ddf1651a1a1898
Mikalai Shurkaeu <mshurkaeu.public@gmail.com>
2026-04-11 15:46:22 +0200

    Upgrade Flutter package package_info_plus to version 10.0.0
    just because the new version is available. And record my gratitude
    to Flutter community for creation of the version of the package.

--- i-care.by-app/lib/my_gratitude.dart
+++ i-care.by-app/lib/my_gratitude.dart
@@ -44,6 +44,9 @@ class MyGratitude extends StatelessWidget {
                 style: titleStyle,
               ),
               TextSpan(
+                text: l10n.aboutMyGratitude_2026_04_11_01,
+              ),
+              TextSpan(
                 text: l10n.aboutMyGratitude_2026_04_03_01,
               ),
               TextSpan(
